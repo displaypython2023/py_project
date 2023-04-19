ab044dd1030e0fbace97d0e4e10965d909fb66c5
Stagiaire <Stagiaire@DESKTOP-BRU3ORB.localdomain>
2023-04-19 14:51:42 +0200

    request

--- training/samples/request_api.py
+++ training/samples/request_api.py
@@ -1,10 +1,21 @@
 import requests
 import json
-r = requests.get('https://httpbin.org/basic-auth/user/pass', auth=('user', 'pass'))
-r.status_code
-r.headers['content-type']
-r.encoding
-r.text
-pretty_json =json.dumps(r.json(),indent=2)
-print(f'Pretty: {pretty_json}')
-
+import urllib.request
+from PIL import Image
+r = requests.get('https://dog.ceo/api/breeds/image/random')
+if r.status_code == 200:
+    try:
+        pretty_json =json.dumps(r.json(),indent=2)
+    except:
+        print("not a json")
+        exit()
+    print(f'Pretty: {pretty_json}')
+else:
+    print(f"error {r.status_code}")
+msg= r.json()
+urllib.request.urlretrieve(
+  msg['message'],
+   "gfg.png")
+  
+img = Image.open("gfg.png")
+img.show()
